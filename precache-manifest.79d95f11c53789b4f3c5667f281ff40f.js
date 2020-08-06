c985157ebf31982c1bbebd0ea5edf4202ee98727
ssbzeref <nascentsurya@gmail.com>
2020-08-06 17:30:05 +0545

    INITIAL COMMIT

---
+++ precache-manifest.79d95f11c53789b4f3c5667f281ff40f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e8175406d101345ebff07df9207c30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb4eeb68c5b0b80c6368",
+    "url": "/static/css/2.01bcce7e.chunk.css"
+  },
+  {
+    "revision": "db6e765c01ac162117a5",
+    "url": "/static/css/main.44c9931f.chunk.css"
+  },
+  {
+    "revision": "cb4eeb68c5b0b80c6368",
+    "url": "/static/js/2.3f67a374.chunk.js"
+  },
+  {
+    "revision": "448b82ad7826ce197831eeb1550dd963",
+    "url": "/static/js/2.3f67a374.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db6e765c01ac162117a5",
+    "url": "/static/js/main.2d1ae33a.chunk.js"
+  },
+  {
+    "revision": "b92d5c8435c4efff2f03",
+    "url": "/static/js/runtime-main.6b7be09e.js"
+  },
+  {
+    "revision": "46a4770393085cc15baff0e3f8f065ac",
+    "url": "/static/media/img1.46a47703.jpg"
+  }
+]);
